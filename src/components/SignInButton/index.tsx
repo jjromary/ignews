d9bc5fcf32ec39c56333e69c380b75d2529b48fc
Jose Romary <joseromarybrandao@gmail.com>
2022-02-01 20:56:14 -0300

    criando user no fauna e verificando duplicidade

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,27 @@
+import { signIn, signOut ,useSession } from 'next-auth/react'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-
-
 import styles from './styles.module.scss'
 
 export function SignInButton(){
+    const { data: session } = useSession()
 
-    const  isUserLoggedIn = true;
-
-    return isUserLoggedIn ? (
-        <button type="button" className={styles.signinbutton}>
+    return session ? (
+        <button
+            type="button" 
+            className={styles.signinbutton}
+            onClick={() => signOut()}
+         >
             <FaGithub  color="#04d361"/>
-            JJROMARY
+            {session.user.name}
             <FiX color='#737380' className={styles.colseIcon}/>
         </button>
     ) : (
-        <button type="button" className={styles.signinbutton}>
+        <button 
+            type="button" 
+            className={styles.signinbutton}
+            onClick={() => signIn('github')}
+            >
             <FaGithub  color="#eba417"/>
             Sign In with Github
         </button>
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth, { Account, Profile, User } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
+import { query as q } from 'faunadb'
+
+interface signInProps {
+  user: User;
+  account: Account;
+  profile: Profile;
+  email: {
+        verificationRequest?: boolean;
+    };
+}
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn ({user, account, profile}: signInProps){
+      const { email } = user
+
+      try{
+        await fauna.query(
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('users_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                {data: {email} }
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('users_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            )
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
+})
